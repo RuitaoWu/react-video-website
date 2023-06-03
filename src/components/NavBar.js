769b7962b4fca3ce36f38abe2e0d7e5d8e2401dd
RuitaoWuSDSU <rwu9937@sdsu.edu>
2023-06-02 21:24:56 -0700

    finished the base

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Logo } from "../utils/constants";
+const NavBar = () => (
+    <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between" }} >
+        <Link to="/" style={{display:"flex",alignItems:"center"}}>
+            <img src={Logo} alt="logo" height={45}/>
+        </Link>
+    </Stack>
+);
+
+export default NavBar
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Feed} from './Feed';
+export {default as NavBar} from './NavBar';
+export {default as SearchResult} from './SearchResult';
+export {default as VideoDetail} from './VideoDetail';
+export {default as ChannelDetail} from './ChannelDetail';
